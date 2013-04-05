e12aecd5260053d393276b8b830154c13e1ceb56
Tim Sell <tim@songkick.com>
2013-04-05 16:09:56 +0100

    only build min

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 bundle install --local --path vendor/bundle
 echo "Running sprockets"
-bundle exec sprockets -Ijavascripts javascripts/songkick-analytics.js > build/songkick-analytics.js
-bundle exec sprockets --js-compressor=uglifier -Ijavascripts javascripts/songkick-analytics.js > build/songkick-analytics.min.js
+bundle exec sprockets --js-compressor=uglifier -Ijavascripts javascripts/songkick-analytics.js -o build/
